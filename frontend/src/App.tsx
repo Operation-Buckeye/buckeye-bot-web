fef173bf7ca275d7bb343a5b028af8420c2f853d
Josh Apt <apt.9@osu.edu>
2024-10-04 20:33:34 -0400

    end of navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -49,65 +49,10 @@ return (
             </ul>
         </aside>
         <main>
-            <div className="content">
-            <ul className="products">
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="images/d1.jpg" alt="product" />
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Stars (10 Reviews)</div>  
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="images/d1.jpg" alt="product" />
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Stars (10 Reviews)</div>  
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="images/d1.jpg" alt="product" />
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Stars (10 Reviews)</div>  
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="images/d1.jpg" alt="product" />
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Stars (10 Reviews)</div>  
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="images/d1.jpg" alt="product" />
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Stars (10 Reviews)</div>  
-                    </div>
-                </li>
-            </ul>
-            </div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/catalog" element ={<Products />} />
+            </Routes>
         </main>
         <footer>
             &copy; 2024 Buckeye Bots
@@ -119,3 +64,5 @@ return (
 }
 
 export default App;
+
+
